67989585de78c978779513a6f968f4a76eed3567
Sunil <mrsunilranabhat@gmail.com>
2023-01-11 14:03:02 +0545

    Day-2 Update

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("Check");
+
+const firstElement = document.querySelectorAll(".panel");
+
+firstElement.forEach((i) =>{
+    i.addEventListener("mouseover",()=>{
+    i.classList.add("active");
+    });
+
+    i.addEventListener("mouseout",()=>{  
+    i.classList.remove("active");
+    }); 
+});
+
+
+
+console.log(firstElement);
